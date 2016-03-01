d7338bf5210178ac9fefb13dc8d5c4c7759d597f
Haskell Camargo <marcelocamargo@linuxmail.org>
2016-03-01 20:56:45 -0300

    Started compiler interface

--- src/quack.c
+++ src/quack.c
@@ -1,8 +1,21 @@
 #include <stdlib.h>
+#include <string.h>
 
 int main(int argc, char** argv)
 {
-  chdir("../");
-  system("make repl mode=ast");
+  char out[200];
+  char src[250];
+
+  strcpy(out, "");
+  strcpy(src, "php ../src/Quack.php ");
+  int i;
+  for (i = 1; i < argc; i++) {
+    strcat(out, " ");
+    strcat(out, argv[i]);
+  }
+
+  strcat(src, out);
+  system(src);
   return 0;
 }
+
