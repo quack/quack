65b5f1ffb74a8e8a48852595ec59df7fd864a192
Haskell Camargo <marcelocamargo@linuxmail.org>
2016-07-03 12:38:22 -0300

    Better syntax error source output

--- src/parser/SyntaxError.php
+++ src/parser/SyntaxError.php
@@ -28,7 +28,7 @@ use \QuackCompiler\Lexer\Token;
 define('BEGIN_RED', "\033[01;31m");
 define('END_RED', "\033[0m");
 
-define('BEGIN_GREEN', "\033[01;33m");
+define('BEGIN_GREEN', "\033[01;32m");
 define('END_GREEN', "\033[0m");
 
 define('BEGIN_BG_RED', "\033[01;41m");
@@ -52,9 +52,32 @@ class SyntaxError extends Exception
 
     private function extractPieceOfSource()
     {
-        // TODO: Apply a better error handler. The column should go (n)
-        // chars back when the expected element does not match
-        return "";
+        // TODO: do-end example not working
+        $out_buffer = [];
+        $position = $this->getPosition();
+        $token_size = $this->getFoundTokenSize();
+        $new_column = $position['column'] - $token_size;
+        $error_line = str_split(
+            explode(PHP_EOL, $this->getOriginalSource()->input)[
+                $position['line'] - 1
+            ]
+        );
+
+        $line_indicator = "{$position['line']}| ";
+
+        $correct_piece = $new_column - 1 <= 0
+            ? []
+            : array_slice($error_line, 0, $new_column);
+
+        $error_piece = array_slice($error_line, $new_column, $new_column + 1);
+
+        $out_buffer[] = $line_indicator;
+        $out_buffer[] = BEGIN_GREEN . implode($correct_piece) . END_GREEN;
+        $out_buffer[] = BEGIN_BG_RED . implode($error_piece) . END_BG_RED;
+        $out_buffer[] = PHP_EOL . str_repeat(' ', strlen($line_indicator) + sizeof($correct_piece));
+        $out_buffer[] = BEGIN_BOLD . str_repeat('^', sizeof($error_piece)) . END_BOLD;
+
+        return implode($out_buffer);
     }
 
     public function __toString()
@@ -70,7 +93,7 @@ class SyntaxError extends Exception
             "    Expecting [{$expected}]",
             "    Found     [{$found}]",
             "    Line      {$position['line']}",
-            "    Column    {$position['column']}",
+            "    Column    " . ($position['column'] - $this->getFoundTokenSize() + 1),
             END_RED
         ]);
     }
@@ -89,6 +112,27 @@ class SyntaxError extends Exception
         return 0 === $found_tag
             ? "end of the source"
             : Tag::getName($found_tag) ?: $found_tag;
+    }
+
+    private function getFoundTokenSize()
+    {
+        if ($this->found instanceof \QuackCompiler\Lexer\Word) {
+            // Keyword found
+            return sizeof($this->found->lexeme);
+        } else {
+            // Operator, literal or EoF found
+            $found_tag = $this->found->getTag();
+            $token_val = $this->parser->input->getSymbolTable()->get(
+                $this->found->getPointer()
+            );
+
+            return 0 === $found_tag
+                ? -1
+                : strlen(null !== $token_val
+                    ? $token_val
+                    : $found_tag
+                );
+        }
     }
 
     private function getOriginalSource()
