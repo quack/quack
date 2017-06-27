765a1d47349fa63160f6ca1c3ee0ac3621962477
Marcelo Camargo <marcelocamargo@linuxmail.org>
2017-06-26 22:37:47 -0300

    text/x-quack mimetype

---
+++ unix/src/configure-mimetype.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+function copy_icon {
+    local SIZE=$1
+    cp icons/$SIZE/text-x-quack.png /usr/share/icons/hicolor/$SIZE/mimetypes
+}
+
+# Copy mimetypes
+cp mimetypes/quack.xml /usr/share/mime/packages
+
+copy_icon '16x16'
+copy_icon '32x32'
+copy_icon '64x64'
+copy_icon '128x128'
+
+# Reload configurations
+update-mime-database /usr/share/mime
+update-icon-caches /usr/share/icons/*
