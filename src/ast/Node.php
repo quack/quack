cbbc776215d045a48fb715e5121b975489ab74d5
Luiz Fernando Peres <luiz_peres10@hotmail.com>
2017-07-11 22:58:29 -0700

    Optimizing the attachment for the AST

--- src/ast/Node.php
+++ src/ast/Node.php
@@ -38,7 +38,7 @@ abstract class Node
 
     public function createScopeWithParent(Scope &$parent)
     {
-        $this->scope = new Scope;
+        $this->scope = new Scope();
         $this->scope->parent = $parent;
     }
 
