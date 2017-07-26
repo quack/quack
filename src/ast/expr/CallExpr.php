35c69fbc2972161b46a852e2a888c85887cc23b6
Marcelo Camargo <marcelocamargo@linuxmail.org>
2017-07-25 23:17:11 -0300

    Type inference for call and lambda expressions

--- src/ast/expr/CallExpr.php
+++ src/ast/expr/CallExpr.php
@@ -21,22 +21,25 @@
  */
 namespace QuackCompiler\Ast\Expr;
 
+use \QuackCompiler\Ast\Types\FunctionType;
+use \QuackCompiler\Intl\Localization;
 use \QuackCompiler\Parser\Parser;
+use \QuackCompiler\Types\TypeError;
 
 class CallExpr extends Expr
 {
-    public $func;
+    public $callee;
     public $arguments;
 
-    public function __construct($func, $arguments)
+    public function __construct($callee, $arguments)
     {
-        $this->func = $func;
+        $this->callee = $callee;
         $this->arguments = $arguments;
     }
 
     public function format(Parser $parser)
     {
-        $source = $this->func->format($parser);
+        $source = $this->callee->format($parser);
         $source .= '(';
         $source .= implode(', ', array_map(function (Expr $arg) use ($parser) {
             return $arg->format($parser);
@@ -48,10 +51,40 @@ class CallExpr extends Expr
 
     public function injectScope(&$parent_scope)
     {
-        $this->func->injectScope($parent_scope);
+        $this->callee->injectScope($parent_scope);
 
         foreach ($this->arguments as $arg) {
             $arg->injectScope($parent_scope);
         }
+    }
+
+    public function getType()
+    {
+        $callee_type = $this->callee->getType();
+        if (!($callee_type instanceof FunctionType)) {
+            throw new TypeError(Localization::message('TYP310', [$callee_type]));
+        }
+
+        // Check parameters length
+        $expected_arguments = sizeof($callee_type->parameters);
+        $received_arguments = sizeof($this->arguments);
+
+        if ($received_arguments !== $expected_arguments) {
+            throw new TypeError(Localization::message('TYP320',
+                [$callee_type, $expected_arguments, $received_arguments]));
+        }
+
+        // Check for each parameter type based on index
+        for ($i = 0; $i < $expected_arguments; $i++) {
+            $expected_type = $callee_type->parameters[$i];
+            $received_type = $this->arguments[$i]->getType();
+
+            if (!$expected_type->check($received_type)) {
+                throw new TypeError(Localization::message('TYP330',
+                    [$i + 1, $expected_type, $received_type]));
+            }
+        }
+
+        return $callee_type->return;
     }
 }
